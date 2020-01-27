72d9f03066a152bbe848a56acee6f0f75cb3562b
rlucente-se-jboss <rlucente@redhat.com>
2020-01-27 17:23:19 -0500

    make sure readable

--- config-fips-java.sh
+++ config-fips-java.sh
@@ -9,8 +9,10 @@ PASSFILE=$WORKDIR/password.internal
 echo 'admin1jboss!' > $PASSFILE
 
 certutil -N -d $WORKDIR/nssdb -f $PASSFILE
-certutil -A -d $WORKDIR/nssdb -a -n rootca -i $WORKDIR/certs/ca.cert.pem -t CT,C,C -f $PASSFILE
-certutil -A -d $WORKDIR/nssdb -a -n subrootca -i $WORKDIR/certs/intermediate.cert.pem -t CT,C,C -f $PASSFILE
+certutil -A -d $WORKDIR/nssdb -a -n rootca \
+  -i $WORKDIR/certs/ca.cert.pem -t CT,C,C -f $PASSFILE
+certutil -A -d $WORKDIR/nssdb -a -n subrootca \
+  -i $WORKDIR/certs/intermediate.cert.pem -t CT,C,C -f $PASSFILE
 pk12util -i certs/server.p12 -d $WORKDIR/nssdb -k $PASSFILE -w $PASSFILE
 certutil -L -d $WORKDIR/nssdb -h all
 certutil -K -d $WORKDIR/nssdb -h all -f $PASSFILE
